d88dc541423ff5f0dd9bbe4755413262e6a1f523
unknown <derek.yipkwokchun@gmail.com>
2022-09-27 22:31:07 +0800

    add ComparsionSlider Component add logo

--- src/Components/ComparsionSlider/ComparsionSlider.jsx
+++ src/Components/ComparsionSlider/ComparsionSlider.jsx
@@ -8,6 +8,9 @@ function ComparsionSlider() {
   return (
     <div className='ComparsionSlider_wrapper center_center' style={{"--slide":'500'}}>
         <div className='ComparsionSlider_inner'>
+            <div className='logo_cover'>
+                <img className="logo" src="https://www.nicepng.com/png/full/162-1628487_plumeria-flower-logo-vector-png-frangipani-logo.png" alt="" />
+            </div>
             <input type="range" min='1' max='1000' defaultValue='500' className='ComparsionSlider' onInput={(e)=>SyncValue(e.target)}/>
         </div>
     </div>
