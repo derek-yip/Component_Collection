6ecb650bd1c767503369f91c97e681a8f31dcdc1
unknown <derek.yipkwokchun@gmail.com>
2022-09-30 00:48:16 +0800

    add ButtonList component add second button

--- src/Components/ButtonList/ButtonList.jsx
+++ src/Components/ButtonList/ButtonList.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ButtonList.scss'
 function ButtonList() {
+
+  const [click,setClick] = useState(false);
+
+  const button3=(self)=>{
+    setClick(!click);
+    self.style.transform = 'rotateY(360deg)';
+    if (click) {
+      self.style.background = 'lightblue'
+    }else{
+      self.style.background = 'lightblue'
+    }
+  }
+
   return (
     <div className="ButtonList">
         <button id='1' className='cover'> ClickMe <button className='inner'>ClickMe</button></button>
-        <button id='2'> ClickMe</button>
-        <button id='3'> ClickMe</button>
+        <button id='2' className='cover'> ClickMe <button className='inner'>ClickMe</button></button>
+        <button id='3' onClick={(e)=>button3(e.target)}> ClickMe</button>
         <button id='4'> ClickMe</button>
         <button id='5'> ClickMe</button>
         <button id='6'> ClickMe</button>
